b8c64e7c67ea0f40a27902cfe1ff62ed6f250960
mh123123111 <46865660+mh123123111@users.noreply.github.com>
2025-07-04 14:06:11 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -102,7 +102,7 @@ const CHARS: &[char] = &[
 
 // pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.3.20"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RS_PUB_KEY: &str = "AjlPTjAbhX38E9c8+k1C7O0E8Sgkst2qnUWuoSmIRSQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
